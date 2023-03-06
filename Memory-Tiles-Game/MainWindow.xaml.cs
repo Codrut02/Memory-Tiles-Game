06f09985fc808fea9767160ccd722f793ca63aed
Codrut02 <codrutpavel02@gmail.com>
2023-03-06 15:27:43 +0200

    First commit

---
+++ Memory-Tiles-Game/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Memory_Tiles_Game
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
