5ca4767805b0d5084d8b144a1a11688879f2821f
Codrut02 <codrutpavel02@gmail.com>
2023-03-14 16:55:10 +0200

    Finished first page (register page) where user can either choose a profile or create his own by entering a name and choosing preffered avatar

--- Memory-Tiles-Game/MainWindow.xaml.cs
+++ Memory-Tiles-Game/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace Memory_Tiles_Game
 {
@@ -8,24 +12,60 @@ namespace Memory_Tiles_Game
         public MainWindow()
         {
             InitializeComponent();
+            profilesViewList.ItemsSource = Profiles;
+            imageViewList.ItemsSource = AvatarCollection;
         }
 
-        private void bNewUser_Click(object sender, RoutedEventArgs e)
+        private readonly List<string> AvatarCollection = new List<string>
+        {
+            "C:\\Users\\codru.LAPTOP-F7RR2UR3\\Documents\\Memory-Tiles-Game\\Avatars\\bull.png",
+            "C:\\Users\\codru.LAPTOP-F7RR2UR3\\Documents\\Memory-Tiles-Game\\Avatars\\cat.png",
+            "C:\\Users\\codru.LAPTOP-F7RR2UR3\\Documents\\Memory-Tiles-Game\\Avatars\\cow.png",
+            "C:\\Users\\codru.LAPTOP-F7RR2UR3\\Documents\\Memory-Tiles-Game\\Avatars\\elephant.png",
+            "C:\\Users\\codru.LAPTOP-F7RR2UR3\\Documents\\Memory-Tiles-Game\\Avatars\\lion.png",
+            "C:\\Users\\codru.LAPTOP-F7RR2UR3\\Documents\\Memory-Tiles-Game\\Avatars\\mouse.png",
+            "C:\\Users\\codru.LAPTOP-F7RR2UR3\\Documents\\Memory-Tiles-Game\\Avatars\\pig.png",
+            "C:\\Users\\codru.LAPTOP-F7RR2UR3\\Documents\\Memory-Tiles-Game\\Avatars\\racoon.png",
+            "C:\\Users\\codru.LAPTOP-F7RR2UR3\\Documents\\Memory-Tiles-Game\\Avatars\\thingy.png",
+            "C:\\Users\\codru.LAPTOP-F7RR2UR3\\Documents\\Memory-Tiles-Game\\Avatars\\tiger.png"
+        };
+
+        private ObservableCollection<Profile> Profiles { get; set; } = new ObservableCollection<Profile>
+        {
+            new Profile("Gigi", "C:\\Users\\codru.LAPTOP-F7RR2UR3\\Documents\\Memory-Tiles-Game\\Avatars\\bull.png"),
+            new Profile("Codrut", "C:\\Users\\codru.LAPTOP-F7RR2UR3\\Documents\\Memory-Tiles-Game\\Avatars\\cat.png"),
+            new Profile("mara", "C:\\Users\\codru.LAPTOP-F7RR2UR3\\Documents\\Memory-Tiles-Game\\Avatars\\cow.png"),
+            new Profile("George", "C:\\Users\\codru.LAPTOP-F7RR2UR3\\Documents\\Memory-Tiles-Game\\Avatars\\pig.png"),
+            new Profile("susu", "C:\\Users\\codru.LAPTOP-F7RR2UR3\\Documents\\Memory-Tiles-Game\\Avatars\\tiger.png")
+        };
+
+        private void BNewUser_Click(object sender, RoutedEventArgs e)
+        {
+            string avatarPathDestination;
+
+            if (imageViewList.SelectedItem == null)
+            {
+                avatarPathDestination = "C:\\Users\\codru.LAPTOP-F7RR2UR3\\Documents\\Memory-Tiles-Game\\Avatars\\bull.png";
+            }
+            else
+            {
+                avatarPathDestination = (string)imageViewList.SelectedItem;
+            }
+
+            Profiles.Add(new Profile(textBox1.Text, avatarPathDestination));
+        }
+
+        private void BDeleteUser_Click(object sender, RoutedEventArgs e)
+        {
+            Profiles.Remove(profilesViewList.SelectedItem as Profile);
+        }
+
+        private void BPlay_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void bDeleteUser_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void bPlay_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void bExit_Click(object sender, RoutedEventArgs e)
+        private void BExit_Click(object sender, RoutedEventArgs e)
         {
             Environment.Exit(1);
         }
---
+++ Memory-Tiles-Game/Profile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory_Tiles_Game
+{
+    public class Profile
+    {
+        public string Name { get; set; }
+
+        public string AvatarDestination { get; set; }
+
+        public Profile(string name, string avatarDestination)
+        {
+            Name = name;
+            AvatarDestination = avatarDestination;
+        }
+    }
+}
